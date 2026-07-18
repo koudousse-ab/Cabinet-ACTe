a5f484d837aae710fb169d8b7f413701be74e99e
TCHEDRE koudousse <koudousetchedre@gmail.com>
2026-07-18 17:10:45 +0000

    Create main application class

---
+++ backend/src/main/java/com/cabinet/acte/CabinetActeApplication.java
@@ -0,0 +1,11 @@
+package com.cabinet.acte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetActeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CabinetActeApplication.class, args);
+    }
+}
